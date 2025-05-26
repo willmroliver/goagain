bb4f3ee97554ae790a641d8158a4cc57a48e02dc
Will Oliver <willoliver.main@gmail.com>
2025-05-26 15:24:24 +0100

    add WebSocket Protocol frame struct to support encoding/decoding of correct binary wire-format

---
+++ src/websocket/frame.go
@@ -0,0 +1,160 @@
+package websocket
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/binary"
+	"errors"
+	"math"
+)
+
+const (
+	FrameOpcodeCont   = 0x0
+	FrameOpcodeText   = 0x1
+	FrameOpcodeBinary = 0x2
+	FrameOpcodeClose  = 0x8
+	FrameOpcodePing   = 0x9
+	FrameOpcodePong   = 0xA
+)
+
+var ErrBadFrame = errors.New("malformed frame wire-format")
+
+type Frame struct {
+	Payload    []byte
+	Final      bool
+	Opcode     byte
+	Masked     bool
+	MaskingKey [4]byte
+}
+
+// NewMaskingKey generates a 32-bit cryptographically
+// secure key for masking and unmasking client frames
+func (f *Frame) NewMaskingKey() {
+	rand.Read(f.MaskingKey[:])
+}
+
+// ApplyMask will mask an unmasked payload, or unmask
+// a masked payload, as the WebSocket Protocol masking
+// algrithm is its own inverse
+func (f *Frame) ApplyMask() {
+	for i := range f.Payload {
+		f.Payload[i] ^= f.MaskingKey[i%4]
+	}
+
+	f.Masked = !f.Masked
+}
+
+// Encode serializes a WebSocket Protocol frame in accordance with
+// [RFC6455] and ABNF [RFC5234].
+func (f *Frame) Encode() (data []byte, err error) {
+	// FIN + RSV1-3 + opcode + MASK + Payload len
+	n := 2
+
+	// Extended payload len
+	pl, epl := len(f.Payload), 0
+
+	if 125 < pl && pl <= math.MaxUint16 {
+		epl = 2
+	} else if math.MaxUint16 < pl {
+		epl = 8
+	}
+
+	n += epl + pl
+
+	// Masking key
+	if f.Masked {
+		n += 4
+	}
+
+	buf := new(bytes.Buffer)
+	buf.Grow(n)
+
+	var b [2]byte
+	if f.Final {
+		b[0] = 1
+	}
+
+	b[0] = (b[0] << 7) | f.Opcode
+	if f.Masked {
+		b[1] = (1 << 7)
+	}
+
+	switch {
+	case epl == 0:
+		b[1] |= byte(pl)
+		buf.Write(b[:])
+	case epl == 2:
+		b[1] |= 126
+		buf.Write(b[:])
+		err = binary.Write(buf, binary.BigEndian, uint16(pl))
+	case epl == 8:
+		b[1] |= 127
+		buf.Write(b[:])
+		err = binary.Write(buf, binary.BigEndian, uint64(pl))
+	default:
+		panic("invalid payload len!")
+	}
+
+	if err != nil {
+		return
+	}
+
+	if f.Masked {
+		buf.Write(f.MaskingKey[:])
+	}
+
+	buf.Write(f.Payload)
+	data = buf.Bytes()
+
+	return
+}
+
+// Decode parses WebSocket Protocol frame bytes in accordance with
+// [RFC6455] and ABNF [RFC5234].
+//
+// f.Payload is copied from data, so mutations to data
+// after decoding do not affect f after Decode.
+//
+// If f.Payload is masked, Decode sets f.Masked and does not ApplyMask
+func (f *Frame) Decode(data []byte) (err error) {
+	n := len(data)
+	if n < 2 {
+		err = ErrBadFrame
+		return
+	}
+
+	pstart := 2
+
+	f.Final = data[0]>>7 == 1
+	f.Opcode = data[0] & 0x7
+
+	pl := int(data[1] & 0x7)
+	if 125 < pl && pl <= math.MaxUint16 {
+		pstart += 2
+		_, err = binary.Decode(data[2:4], binary.BigEndian, &pl)
+	} else if math.MaxUint16 < pl {
+		pstart += 8
+		// @todo - proper handling of payloads > 2^32?
+		_, err = binary.Decode(data[2:10], binary.BigEndian, &pl)
+	}
+
+	if err != nil {
+		return
+	}
+
+	if n < pstart+pl {
+		err = ErrBadFrame
+		return
+	}
+
+	if data[1]&0x8 != 0 {
+		f.Masked = true
+		pstart += 4
+		copy(f.MaskingKey[:], data[pstart-4:pstart])
+	}
+
+	f.Payload = make([]byte, pl)
+	copy(f.Payload, data[pstart:pl])
+
+	return
+}
---
+++ src/websocket/frame_test.go
@@ -0,0 +1,121 @@
+package websocket_test
+
+import (
+	"bytes"
+	"encoding/binary"
+	"math"
+	"slices"
+	"testing"
+
+	"github.com/willmroliver/goagain/src/websocket"
+)
+
+func TestApplyMask(t *testing.T) {
+	payload := []byte{1, 1, 0, 0, 2, 2, 4, 4}
+
+	var f websocket.Frame
+
+	f.Payload = payload
+	f.MaskingKey = [4]byte{1, 0, 2, 0}
+
+	f.ApplyMask()
+	exp := []byte{
+		1 ^ 1, 1 ^ 0, 0 ^ 2, 0 ^ 0,
+		2 ^ 1, 2 ^ 0, 4 ^ 2, 4 ^ 0,
+	}
+
+	if !slices.Equal(exp, f.Payload) {
+		t.Errorf("exp %+v, got %+v\n", exp, f.Payload)
+	}
+
+	f.ApplyMask()
+
+	if !slices.Equal(payload, f.Payload) {
+		t.Errorf("exp %+v, got %+v", payload, f.Payload)
+	}
+}
+
+func TestEncode(t *testing.T) {
+	t.Run("Simple frame", func(t *testing.T) {
+		f := &websocket.Frame{
+			Payload: []byte{1, 1, 2, 2, 3, 3, 4, 4},
+			Final:   true,
+			Opcode:  websocket.FrameOpcodeBinary,
+		}
+
+		exp := []byte{(1 << 7) | f.Opcode, 8}
+		exp = append(exp, f.Payload...)
+
+		if got, _ := f.Encode(); !slices.Equal(exp, got) {
+			t.Errorf("exp %+v, got %+v", exp, got)
+		}
+	})
+
+	t.Run("Extended payload", func(t *testing.T) {
+		f := &websocket.Frame{
+			Payload: bytes.Repeat([]byte{1, 2, 3, 4, 1, 2, 3, 4}, 256),
+			Final:   false,
+			Opcode:  websocket.FrameOpcodeText,
+		}
+
+		epl := make([]byte, 2)
+		binary.Encode(epl, binary.BigEndian, uint16(len(f.Payload)))
+
+		exp := []byte{f.Opcode, 126}
+		exp = append(exp, epl...)
+		exp = append(exp, f.Payload...)
+
+		if got, _ := f.Encode(); !slices.Equal(exp, got) {
+			t.Errorf(
+				"2 byte EPL - exp %+v, got %+v",
+				exp[:min(len(exp), 100)],
+				got[:min(len(got), 100)],
+			)
+			return
+		}
+
+		f.Payload = bytes.Repeat([]byte{1}, math.MaxUint16*2)
+		epl = make([]byte, 8)
+		binary.Encode(epl, binary.BigEndian, uint64(len(f.Payload)))
+
+		exp = []byte{f.Opcode, 127}
+		exp = append(exp, epl...)
+		exp = append(exp, f.Payload...)
+
+		if got, _ := f.Encode(); !slices.Equal(exp, got) {
+			t.Errorf(
+				"8-byte EPL - exp %+v, got %+v",
+				exp[:min(len(exp), 100)],
+				got[:min(len(got), 100)],
+			)
+			return
+		}
+	})
+
+	t.Run("Masked payload", func(t *testing.T) {
+		payload := []byte{1, 1, 0, 0, 2, 2, 4, 4}
+
+		f := &websocket.Frame{
+			Payload:    payload,
+			Opcode:     websocket.FrameOpcodeCont,
+			MaskingKey: [4]byte{1, 0, 2, 0},
+		}
+
+		f.ApplyMask()
+
+		exp := []byte{
+			f.Opcode,
+			(1 << 7) | byte(len(payload)),
+		}
+		exp = append(exp, f.MaskingKey[:]...)
+		exp = append(exp, []byte{
+			1 ^ 1, 1 ^ 0, 0 ^ 2, 0 ^ 0,
+			2 ^ 1, 2 ^ 0, 4 ^ 2, 4 ^ 0,
+		}...)
+
+		if got, _ := f.Encode(); !slices.Equal(exp, got) {
+			t.Errorf("exp %+v, got %+v", exp, got)
+			return
+		}
+	})
+}
